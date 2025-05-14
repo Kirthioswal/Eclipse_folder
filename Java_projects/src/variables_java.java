e8d09232ff896e2aadeddda0af7cd89069c80624
Kirthioswal <oswalkirti2@gmail.com>
2025-05-14 21:48:05 +0530

    New changes made

---
+++ Java_projects/src/variables_java.java
@@ -0,0 +1,20 @@
+
+public class variables_java {
+public static void main(String[] args) {
+	int a=10;
+	float b=13.5f;
+	double c=464747;
+	char d='A';
+	String S="WELCOME";
+	long l=2344l;
+	boolean Z= true;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(S);
+	System.out.println(l);
+	System.out.println(Z);
+}
+	
+}
