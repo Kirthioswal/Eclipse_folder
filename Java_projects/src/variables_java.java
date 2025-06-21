12fb12783b851c1cd49fd0ced90ae9eb7d6ab3d3
Kirthioswal <oswalkirti2@gmail.com>
2025-06-21 20:53:19 +0530

    variables_literal

--- Java_projects/src/variables_java.java
+++ Java_projects/src/variables_java.java
@@ -15,6 +15,16 @@ public static void main(String[] args) {
 	System.out.println(S);
 	System.out.println(l);
 	System.out.println(Z);
+	int _a = 45;
+    int are_$ = 54;
+    int $ = 60;
+    int vaiable_name_descritpion_long_format_name_12345_657_Lognhf_ = 123;
+    /* it wont give u error if u use _ as identifier but it wont give u output of that indentifier
+    int _ = 34; */
+    System.out.println(_a);
+    System.out.println(are_$);
+    System.out.println(vaiable_name_descritpion_long_format_name_12345_657_Lognhf_);
+    System.out.println($);
 }
 	
 }
